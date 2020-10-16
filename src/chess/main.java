f83b5b833ab7d6598b08eda8e0efa03893c0493c
ChaserZ98 <feiyuzheng98@gmail.com>
2020-10-16 14:30:59 -0400

    test commit of java file

---
+++ src/chess/main.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the test commit of chess project");
+
+	}
+
+}
