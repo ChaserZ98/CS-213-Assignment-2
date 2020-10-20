1b638e403236edddbd3612bed6c1c8a34965804b
Ying Yu <yy465@scarletmail.rutgers.edu>
2020-10-20 14:49:38 -0400

    define all classes

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -11,8 +11,6 @@ public class Chess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is the test commit of chess project");
-		System.out.println("Test submit from IDEA!");
 
 	}
 
