6c1919ee5a7dda3d4fe93ea83756bff86f68ed35
ChaserZ98 <fz114@rutgers.edu>
2020-10-22 09:53:48 -0400

    update the basic logic of a chess game

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,7 +1,9 @@
 
 package chess;
 
-import model.board;
+import model.Board;
+
+import java.util.Scanner;
 
 /**
  * Group 13
@@ -14,13 +16,68 @@ import model.board;
 public class Chess {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        final String whitePlayer = "White";
+        final String blackPlayer = "Black";
+        String currentPlayer = whitePlayer;
 
-        board chessBoard = new board();
+        Board chessBoard = new Board();
+        boolean isEnd  = false;
 
-        System.out.println("");
-        board.drawBoardLayout();
-        System.out.println("");
+        Scanner input = new Scanner(System.in);
+
+        while(!isEnd){
+            System.out.println("");
+            System.out.println(chessBoard);
+            System.out.println("");
+            System.out.print(currentPlayer + "'s Move: ");
+            String command = input.nextLine();
+
+            /*
+            resign
+             */
+            if(command.equals("resign")){
+                if(currentPlayer.equals(blackPlayer)){
+                    System.out.println(whitePlayer + " wins");
+                }
+                else{
+                    System.out.println(blackPlayer + " wins");
+                }
+                isEnd = true;
+            }
+            else{
+                String[] parameter = command.split(" ");
+                /*
+                regular move
+                 */
+                if(parameter.length == 2){
+                    System.out.println("Input: " + parameter[0] + " " + parameter[1]);
+                }
+                /*
+                regular move with appended option
+                 */
+                else if(parameter.length == 3){
+                    if(parameter[2].equals("N")){
+                        System.out.println("Pawn promotion");
+                    }
+                    else if(parameter[2].equals("draw?")){
+                        System.out.println("draw request");
+                    }
+                    else{
+                        System.out.println("Illegal command.");
+                    }
+                }
+                /*
+                other illegal commands
+                 */
+                else{
+                    System.out.println("Illegal command.");
+                }
+            }
+
+            currentPlayer = currentPlayer.equals(blackPlayer)? whitePlayer : blackPlayer;
+        }
+        input.close();
+
 
     }
 
