ab8543c81eb182e3938fa64a0b61c54f473f9119
ChaserZ98 <fz114@rutgers.edu>
2020-10-22 00:49:50 -0400

    correct the class name

--- src/model/Board.java
+++ src/model/Board.java
@@ -12,11 +12,11 @@ import control.control;
  *
  */
 
-public class board {
+public class Board {
 
     private static CommonPiece[][] boardLayout;
 
-    public board() {
+    public Board() {
 
         boardLayout = new CommonPiece[8][8];
 
@@ -33,25 +33,25 @@ public class board {
      * ##    ##    ##    ##    3
      * wp wp wp wp wp wp wp wp 2
      * wR wN wB wQ wK wB wN wR 1
-     * a   b  c  d  e  f  g  h
+     *  a  b  c  d  e  f  g  h
      *
      * Fill boardLayout for the first time:  8x8 (index 0 to 7)
      */
 
     public void boardLayoutFill() {
 
-        /**
-         * Firstly set 8x8 pieces to null
-         *
+        /*
+          Firstly set 8x8 pieces to null
+
          */
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 boardLayout[i][j] = null;
             }
         }
-        /**
-         * set all black pieces
-         *
+        /*
+          set all black pieces
+
          */
         boardLayout[0][0] = new Rook("a8", "black");
         boardLayout[0][1] = new Knight("b8", "black");
@@ -60,18 +60,18 @@ public class board {
         boardLayout[0][4] = new King("e8", "black");
         boardLayout[0][7] = new Rook("h8", "black");
         boardLayout[0][5] = new Bishop("f8", "black");
-        boardLayout[0][6]  = new Knight("g8", "black");
+        boardLayout[0][6] = new Knight("g8", "black");
         for (int i = 0; i < 8; i++)
             boardLayout[1][i] = new Pawn(control.intToChar(i) + "7", "black");
-        /**
-         * set all white pieces
-         *
+        /*
+          set all white pieces
+
          */
-        boardLayout[7][0]  = new Rook("a1", "white");
+        boardLayout[7][0] = new Rook("a1", "white");
         boardLayout[7][1] = new Knight("b1", "white");
         boardLayout[7][2] = new Bishop("c1", "white");
-        boardLayout[7][3]  = new Queen("d1", "white");
-        boardLayout[7][4]= new King("e1", "white");
+        boardLayout[7][3] = new Queen("d1", "white");
+        boardLayout[7][4] = new King("e1", "white");
         boardLayout[7][5] = new Bishop("f1", "white");
         boardLayout[7][6] = new Knight("g1", "white");
         boardLayout[7][7] = new Rook("h1", "white");
@@ -87,29 +87,30 @@ public class board {
     public static void drawBoardLayout() {
 
         String[][] tempBoard = new String[9][9];
-        /**
-         * Fill board from [0][0] to [7][7] for pieces
-         *
+        /*
+          Fill board from [0][0] to [7][7] for pieces
+
          */
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (boardLayout[i][j] != null) {
                     tempBoard[i][j] = boardLayout[i][j].toString();
-                } else {
-
+                }
+                else {
                     if ((i % 2 == 1 && j % 2 == 0) || (i % 2 == 0 && j % 2 == 1)) {
                         tempBoard[i][j] = "##";
-                    } else {
+                    }
+                    else {
                         tempBoard[i][j] = "  ";
                     }
                 }
             }
         }
 
-        /**
-         * Draw 8 to 1 from [0][8] to [7][8]
-         *Draw a to h from [8][0] to [8][7]
-         * [8][8] is space
+        /*
+          Draw 8 to 1 from [0][8] to [7][8]
+         Draw a to h from [8][0] to [8][7]
+          [8][8] is space
          */
         for (int i = 0; i < 8; i++)
             tempBoard[i][8] = Integer.toString(8 - i);
@@ -118,9 +119,9 @@ public class board {
 
         tempBoard[8][8] = "  ";
 
-        /**
-         * Print for output from [0][0] to [8][8]
-         *
+        /*
+          Print for output from [0][0] to [8][8]
+
          */
         for (int i = 0; i < 9; i++) {
             String eachBoardLine = "";
