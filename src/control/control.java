68f4eff44ab18810f696fc33327ee2a6758deafc
ChaserZ98 <fz114@rutgers.edu>
2020-10-24 04:32:44 -0400

    created letterCoordinateToIntCoordinate() method to transfer letter coordinate to integer index.

--- src/control/control.java
+++ src/control/control.java
@@ -18,4 +18,15 @@ public class control {
         return (char) (i+97);
     }
 
+    /**
+     *
+     * @param letterCoordinate position of piece represented by letter coordinate
+     * @return intCoordinate
+     */
+    public static int[] letterCoordinateToIntCoordinate(String letterCoordinate){
+        int[] intCoordinate = new int[2];
+        intCoordinate[1] = letterCoordinate.charAt(0) - 'a';
+        intCoordinate[0] = 8 - (letterCoordinate.charAt(1) - '0');
+        return intCoordinate;
+    }
 }
