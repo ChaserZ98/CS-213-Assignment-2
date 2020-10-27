7e03daafd6f639127a33d2f88e5d33d0bc930d1c
ChaserZ98 <fz114@rutgers.edu>
2020-10-26 22:42:38 -0400

    corrected bug

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -21,7 +21,7 @@ public class Bishop extends CommonPiece {
     public boolean checkMoveRange(String destination) {
         int[] intCurrentCoordinate = control.letterCoordinateToIntCoordinate(this.currentPosition);
         int[] intDestination = control.letterCoordinateToIntCoordinate(destination);
-        return !destination.equals(this.currentPosition) && Math.abs(intCurrentCoordinate[0] - intDestination[0]) == Math.abs(intCurrentCoordinate[1] - intDestination[1]);
+        return !destination.equals(this.currentPosition) && (Math.abs(intCurrentCoordinate[0] - intDestination[0]) == Math.abs(intCurrentCoordinate[1] - intDestination[1]));
     }
 
     @Override
